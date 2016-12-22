2b7c9e2461348fb3a76a03c172679d42553f8af5
Louis MENIGAULT <lmenigau@e1r1p7.42.fr>
2016-12-22 18:04:49 +0100

    start file parsing

--- fdf.c
+++ fdf.c
@@ -6,7 +6,7 @@
 /*   By: lmenigau <lmenigau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/16 03:18:19 by lmenigau          #+#    #+#             */
-/*   Updated: 2016/12/21 05:57:51 by lmenigau         ###   ########.fr       */
+/*   Updated: 2016/12/21 09:37:34 by lmenigau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,24 +39,8 @@ int		ft_abs(int n)
 	return (n);
 }
 
-int		main()
+void	variousttest(int (*imgmem)[1000])
 {
-	int		(*imgmem)[1000];
-	int		bits_per_pixel;
-	int		size_line;
-	int		endian;
-	void	*mlx_ptr;
-	void	*window;
-	void	*img_ptr;
-
-	mlx_ptr = mlx_init();
-	window = mlx_new_window(mlx_ptr, 1000, 1000, "fdf");
-	mlx_key_hook(window, key_hook, NULL);
-	img_ptr = mlx_new_image(mlx_ptr, 1000, 1000);
-	bits_per_pixel = 32;
-	size_line = 1000;
-	endian = 1;
-	imgmem = (int (*)[1000])mlx_get_data_addr(img_ptr, &bits_per_pixel, &size_line, &endian);
 	imgmem[50][50] = 0x00F1FFFF;
 	plotline(imgmem, 400, 10, 130, 200);
 	plotline(imgmem, 200, 10, 300, 130);
@@ -65,6 +49,57 @@ int		main()
 	plotline(imgmem, 0, 0, 300, 300);
 	plotline(imgmem, 500, 0, 0, 500);
 	plotline(imgmem, 500, 0, 0, 300);
+}
+
+int		**parse_file(int argc, char **argv)
+{
+	int		fd;
+	char	*line;
+	int		**map;
+	char	**tab;
+	int		i;
+	int		j;
+
+	map = malloc(sizeof(*map) * 1000);
+	if (argc < 2)
+		return (0);
+	fd = open(argv[1], O_RDONLY);
+	i = 0;
+	while (get_next_line(fd, &line) == 1)
+	{
+		map[i] = malloc(sizeof (**map) * ft_strlen(line));
+		tab = ft_strsplit(line, ' ');
+		j = 0;
+		while (tab[j])
+		{
+			map[i][j] = ft_atoi(tab[j]);
+			j++;
+		}
+		i++;
+	}
+	return (map);
+}
+
+int		main(int argc, char **argv)
+{
+	int		(*imgmem)[1000];
+	int		bits_per_pixel;
+	int		size_line;
+	int		endian;
+	void	*mlx_ptr;
+	void	*window;
+	void	*img_ptr;
+	int		**map;
+
+	mlx_ptr = mlx_init();
+	window = mlx_new_window(mlx_ptr, 1920, 1080, "fdf");
+	mlx_key_hook(window, key_hook, NULL);
+	img_ptr = mlx_new_image(mlx_ptr, 1000, 1000);
+	bits_per_pixel = 32;
+	size_line = 1000;
+	endian = 1;
+	imgmem = (int (*)[1000])mlx_get_data_addr(img_ptr, &bits_per_pixel, &size_line, &endian);
+	map = parse_file(argc, argv);
 	mlx_put_image_to_window(mlx_ptr, window, img_ptr,0, 0);
 	mlx_mouse_hook(window, mouse_hook, NULL);
 	mlx_loop(mlx_ptr);
--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: lmenigau <lmenigau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/20 23:53:20 by lmenigau          #+#    #+#             */
-/*   Updated: 2016/12/21 03:47:26 by lmenigau         ###   ########.fr       */
+/*   Updated: 2016/12/21 08:21:42 by lmenigau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@
 # define FDF_H
 
 # include <stdio.h>
-# include <stdlib.h>
+# include <fcntl.h>
 # include <mlx.h>
-# include <libft.h>
+# include <stdlib.h>
+# include "libft.h"
+# include "get_next_line.h"
 
 void	plotline(int (*img)[1000], int x0, int y0, int x1, int y1);
 void	plotline_octant(int (*img)[1000], int x0, int y0, int x1, int y1);
