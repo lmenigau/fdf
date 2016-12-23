fa3076a17d63ad1c1ab4e35008c60ead67c5844f
Louis MENIGAULT <lmenigau@e3r7p20.42.fr>
2016-12-23 14:09:19 +0100

    fuck the world

--- fdf.c
+++ fdf.c
@@ -6,7 +6,7 @@
 /*   By: lmenigau <lmenigau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/16 03:18:19 by lmenigau          #+#    #+#             */
-/*   Updated: 2016/12/21 09:37:34 by lmenigau         ###   ########.fr       */
+/*   Updated: 2016/12/23 09:54:47 by lmenigau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ void	variousttest(int (*imgmem)[1000])
 {
 	imgmem[50][50] = 0x00F1FFFF;
 	plotline(imgmem, 400, 10, 130, 200);
-	plotline(imgmem, 200, 10, 300, 130);
+	plotline(imgmem, 20, 10, 300, 130);
 	plotline(imgmem, 100, 10, 300, 130);
 	plotline(imgmem, 100, 10, 130, 300);
 	plotline(imgmem, 0, 0, 300, 300);
@@ -51,33 +51,50 @@ void	variousttest(int (*imgmem)[1000])
 	plotline(imgmem, 500, 0, 0, 300);
 }
 
-int		**parse_file(int argc, char **argv)
+int		parse_file(int argc, char **argv, int ***map)
 {
 	int		fd;
 	char	*line;
-	int		**map;
 	char	**tab;
 	int		i;
 	int		j;
 
-	map = malloc(sizeof(*map) * 1000);
+	*map = malloc(sizeof(**map) * 1000);
 	if (argc < 2)
 		return (0);
 	fd = open(argv[1], O_RDONLY);
 	i = 0;
 	while (get_next_line(fd, &line) == 1)
 	{
-		map[i] = malloc(sizeof (**map) * ft_strlen(line));
+		*map[i] = malloc(sizeof (***map) * ft_strlen(line));
 		tab = ft_strsplit(line, ' ');
 		j = 0;
 		while (tab[j])
 		{
-			map[i][j] = ft_atoi(tab[j]);
+			*map[i][j] = ft_atoi(tab[j]);
 			j++;
 		}
 		i++;
 	}
-	return (map);
+	*map[i] = NULL;
+	return (j);
+}
+
+t_vec2	 **project(int **map, int map_size)
+{
+	int		x;
+	int		y;
+
+	y = 0;
+	while (map[y] != NULL)
+	{
+		x = 0;
+		while (x < map_size)
+		{
+			x++;
+		}
+		y++;
+	}
 }
 
 int		main(int argc, char **argv)
@@ -90,6 +107,8 @@ int		main(int argc, char **argv)
 	void	*window;
 	void	*img_ptr;
 	int		**map;
+	int		map_size;
+	t_vec2	**proj;
 
 	mlx_ptr = mlx_init();
 	window = mlx_new_window(mlx_ptr, 1920, 1080, "fdf");
@@ -99,7 +118,9 @@ int		main(int argc, char **argv)
 	size_line = 1000;
 	endian = 1;
 	imgmem = (int (*)[1000])mlx_get_data_addr(img_ptr, &bits_per_pixel, &size_line, &endian);
-	map = parse_file(argc, argv);
+	variousttest(imgmem);
+	//map_size = parse_file(argc, argv, &map);
+	//proj = project(map, map_size);
 	mlx_put_image_to_window(mlx_ptr, window, img_ptr,0, 0);
 	mlx_mouse_hook(window, mouse_hook, NULL);
 	mlx_loop(mlx_ptr);
--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: lmenigau <lmenigau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/20 23:53:20 by lmenigau          #+#    #+#             */
-/*   Updated: 2016/12/21 08:21:42 by lmenigau         ###   ########.fr       */
+/*   Updated: 2016/12/23 06:59:25 by lmenigau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,19 @@
 # include "libft.h"
 # include "get_next_line.h"
 
-void	plotline(int (*img)[1000], int x0, int y0, int x1, int y1);
-void	plotline_octant(int (*img)[1000], int x0, int y0, int x1, int y1);
-int		key_hook(int keycode, void *param);
-int		expose_hook(void *param);
-int		loop_hook(void *param);
-int		mouse_hook(int button, int x, int y, void *param);
-int		ft_abs(int n);
+typedef struct	s_vec2
+{
+	int			x;
+	int			y;
+}				t_vec2;
+
+void			plotline(int (*img)[1000], int x0, int y0, int x1, int y1);
+void			plotline_octant(int (*img)[1000], int x0, int y0, int x1, int y1);
+int				key_hook(int keycode, void *param);
+int				expose_hook(void *param);
+int				loop_hook(void *param);
+int				mouse_hook(int button, int x, int y, void *param);
+int				ft_abs(int n);
 
 
 #endif
