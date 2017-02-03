8abd18404484f3c556c5a6e5547eab9b812830e7
Louis MENIGAULT <lmenigau@e1r9p9.42.fr>
2017-02-03 18:35:41 +0100

    norme.h

--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: lmenigau <lmenigau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/20 23:53:20 by lmenigau          #+#    #+#             */
-/*   Updated: 2017/02/03 16:15:16 by lmenigau         ###   ########.fr       */
+/*   Updated: 2017/02/03 18:33:36 by lmenigau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,10 @@
 # include "get_next_line.h"
 # include "matrix.h"
 
-#define		WIN_WIDTH	1920
-#define		WIN_HEIGHT	1080
+# define WIN_WIDTH	1920
+# define WIN_HEIGHT	1080
 
-#define		ABS(x)		((x) < 0 ? (-(x)): (x))
+# define ABS(x)		((x) < 0 ? (-(x)): (x))
 
 typedef struct	s_vec2
 {
@@ -67,7 +67,8 @@ typedef struct	s_gstate
 }				t_gstate;
 
 void			plotline(int (*img)[WIN_WIDTH], int x0, int y0, int x1, int y1);
-void			plotline_octant(int (*img)[WIN_WIDTH], int x0, int y0, int x1, int y1);
+void			plotline_octant(int (*img)[WIN_WIDTH], int x0, int y0, int x1,
+		int y1);
 int				key_hook(int keycode, t_gstate *gstate);
 int				expose_hook(void *param);
 int				loop_hook(void *param);
@@ -75,7 +76,8 @@ int				mouse_hook(int button, int x, int y, t_gstate *gstate);
 int				motion_hook(int x, int y, t_gstate *gstate);
 int				ft_abs(int n);
 
-void			plotline_octant_vec(int (*img)[WIN_WIDTH], t_vec2 p0, t_vec2 p1, int fsp);
+void			plotline_octant_vec(int (*img)[WIN_WIDTH], t_vec2 p0, t_vec2 p1,
+		int fsp);
 void			plotline_vec(int (*img)[WIN_WIDTH], t_vec2 p0, t_vec2 p1);
 t_vec2			point_transform(t_vec3 pt, t_m44p m);
 void			map_render(int **map, int line_count, t_gstate *gstate);
--- matrix.h
+++ matrix.h
@@ -6,23 +6,22 @@
 /*   By: lmenigau <lmenigau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/19 14:57:51 by lmenigau          #+#    #+#             */
-/*   Updated: 2017/01/31 02:22:44 by lmenigau         ###   ########.fr       */
+/*   Updated: 2017/02/03 18:35:21 by lmenigau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MATRIX_H
 # define MATRIX_H
 
-
-typedef		float (*t_m44p)[4];
-typedef		float t_m44[4][4];
+typedef	float	(*t_m44p)[4];
+typedef	float	t_m44[4][4];
 
 typedef struct	s_m44
 {
 	t_m44	mat;
 }				t_m44st;
 
-t_m44st		mat_mult(t_m44p l, t_m44p r);
-t_m44st		mat_array_mult(t_m44 *mat_array, int length);
-void		print_mat(t_m44p mat);
+t_m44st			mat_mult(t_m44p l, t_m44p r);
+t_m44st			mat_array_mult(t_m44 *mat_array, int length);
+void			print_mat(t_m44p mat);
 #endif
